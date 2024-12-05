f5c2009dbbae6e2b2940c0d5aebc92dcdad343c7
dhoubrechts <dominique.houbrechts@gmail.com>
2024-12-05 12:16:21 +0100

    Update main.go - localhost connection as a flag
    otherwise it is not possible to make a connection between client and server when running in seperate containers

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
+var baseURL string // Global variable to store the base URL
+
 func main() {
+	// Define the `-baseURL` flag
+	// Example client -baseURL=http://go-example-server
+	flag.StringVar(&baseURL, "baseURL", "http://localhost", "Base URL of the server")
+	flag.Parse()
+
 	err := ping()
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
+
 	err = start()
 	if err != nil {
 		fmt.Println(err.Error())
