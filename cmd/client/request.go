275dc1e293f8b4fa8e76d1eca9946d042a2ae7f8
dhoubrechts <dominique.houbrechts@gmail.com>
2024-12-05 12:17:09 +0100

    Update request.go -  - localhost connection as a flag

--- cmd/client/request.go
+++ cmd/client/request.go
@@ -9,15 +9,19 @@ import (
 	"os"
 )
 
-var baseURL string = "http://localhost"
 var port string = "3000"
-var requestUrl string = fmt.Sprintf("%s:%s?s=", baseURL, port)
+var requestUrl string // Constructed dynamically in ping()
 
 func ping() error {
+	// Construct the request URL dynamically
+	requestUrl = fmt.Sprintf("%s:%s?s=", baseURL, port)
+
 	res, err := http.Get(requestUrl)
 	if err != nil {
 		return err
 	}
+	defer res.Body.Close() // Close response body to avoid leaks
+
 	if res.StatusCode != http.StatusOK {
 		return errors.New("failed to establish server connection")
 	}
@@ -31,6 +35,8 @@ func getTranslation(msg string) string {
 		fmt.Printf("[error] http request failed: %s", err)
 		os.Exit(1)
 	}
+	defer response.Body.Close() // Close response body to avoid leaks
+
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		fmt.Printf("[error] failed to parse server response: %s", err)
